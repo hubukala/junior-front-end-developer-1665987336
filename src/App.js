2e70ab92d8ce614f3774624eea0da7a14ffa2cdc
hubukala <hubertbukala@icloud.com>
2022-10-18 14:17:20 +0200

    added business context component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import TaskList from './components/TaskList';
+import BusinessContext from './components/BusinessContext';
 
 function App() {
   return (
     <div className='App'>
       <TaskList />
+      <BusinessContext />
     </div>
   );
 }
