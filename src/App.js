a5817644b97b748ecca2a2a06ba28bb1384ce982
hubukala <hubertbukala@icloud.com>
2022-10-21 19:11:14 +0200

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import taskListData from './data/task-list.json';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import BusinessContext from './components/BusinessContext';
 import SharedLayout from './shared/SharedLayout';
 import NotFound from './components/NotFound';
-import taskListData from './data/task-list.json';
 
 function App() {
 
-  const [showCurrentTask, setCurrentTask] = useState(taskListData.map((task) => task.title).find((title) => title !== ""))
+  const [showCurrentTask, setCurrentTask] = useState(taskListData.map((task) => task.title).find((title) => title !== ""));
+  const [showCurrentTitle, setCurrentTitle] = useState(" ");
+  const [showMessageAuthor, setMessageAuthor] = useState(" ");
+  const [showMessageAuthorImage, setMessageAuthorImage] = useState("https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/HD_transparent_picture.png/640px-HD_transparent_picture.png");
+  const [showMessageCreated, setMessageCreated] = useState(" ");
+  const [showMessageContent, setMessageContent] = useState(" ");
 
   return (
     <div className='App'>
@@ -16,11 +21,22 @@ function App() {
         <Routes>
           <Route path="/" element=
             {<SharedLayout
+              showCurrentTask={showCurrentTask}
               setCurrentTask={setCurrentTask}
             />}>
             <Route path="/task/:taskId" element=
               {<BusinessContext 
                 showCurrentTask={showCurrentTask}
+                showCurrentTitle={showCurrentTitle}
+                setCurrentTitle={setCurrentTitle}
+                showMessageAuthor={showMessageAuthor}
+                setMessageAuthor={setMessageAuthor}
+                showMessageAuthorImage={showMessageAuthorImage}
+                setMessageAuthorImage={setMessageAuthorImage}
+                showMessageCreated={showMessageCreated}
+                setMessageCreated={setMessageCreated}
+                showMessageContent={showMessageContent}
+                setMessageContent={setMessageContent}
               />}
             />
             <Route path="/*" element={<NotFound/>}/>
@@ -29,6 +45,6 @@ function App() {
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
